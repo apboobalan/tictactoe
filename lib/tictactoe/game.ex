7e66216322be2128359ab637a574bef31a3f3b99
Arther <a.gunaseelan@glofox.com>
2020-03-28 12:55:58 +0530

    add code to handle turns and happy path

--- lib/tictactoe/game.ex
+++ lib/tictactoe/game.ex
@@ -1,12 +1,46 @@
 defmodule Tictactoe.Game do
   alias Tictactoe.UI
   alias Tictactoe.GameState
+  alias Tictactoe.Position
+  alias Tictactoe.Player
 
+  @spec init :: none
   def init() do
-    start(GameState.new)
+    start(GameState.new())
   end
 
-  def start(state) do
-    UI.print(state.matrix)
+  @spec start(any) :: none
+  def start(gamestate = %GameState{}) do
+    UI.print(gamestate.matrix)
+    next_turn(gamestate, false, :A)
+  end
+
+  def next_turn(gamestate = %GameState{}, false, :A) do
+    [x, y] = UI.read_input()
+    handle_turn_result(Player.place(gamestate, %Position{x: x, y: y}, :A), :A, :B)
+  end
+
+  def next_turn(gamestate, false, :B) do
+    [x, y] = UI.read_input()
+    handle_turn_result(Player.place(gamestate, %Position{x: x, y: y}, :B), :B, :A)
+  end
+
+  def next_turn(gamestate = %GameState{}, :won, player) do
+    IO.puts("Player #{player} won")
+  end
+
+  def next_turn(gamestate, true, _) do
+    UI.print(gamestate.matrix)
+  end
+
+
+  defp handle_turn_result({:ok, gamestate} = turnResult, _currentPlayer, nextPlayer) do
+    UI.print(gamestate.matrix)
+    next_turn(gamestate, GameState.done?(gamestate), nextPlayer)
+  end
+
+  defp handle_turn_result({:error, gamestate} = turnResult, currentPlayer, _) do
+    IO.puts("Invalid move")
+    next_turn(gamestate, GameState.done?(gamestate), currentPlayer)
   end
 end
--- lib/tictactoe/ui.ex
+++ lib/tictactoe/ui.ex
@@ -1,21 +1,28 @@
 defmodule Tictactoe.UI do
-  def print(state = []) do
+  @spec print([maybe_improper_list]) :: :ok
+  def print([] = _matrix) do
     IO.puts("Printing Done")
   end
 
-  def print(state) do
-    [current_row | rest] = state
+  def print(matrix) when is_list(matrix) do
+    [current_row | rest] = matrix
     print_row(current_row)
     print(rest)
   end
 
-  defp print_row(row = []) do
+  defp print_row([] = _row) do
     IO.puts("End of Print row")
   end
 
   defp print_row(row) when is_list(row) do
-    [element | rest] = row
     IO.inspect(row)
-    # print_row(rest)
+  end
+
+  @spec read_input :: [binary]
+  def read_input() do
+    IO.gets("Input the position by Row,Column formate. Ex 1,1 or 1,2")
+    |> String.replace("\n", "")
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
   end
 end
